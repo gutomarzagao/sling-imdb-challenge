1f4e9986a81443bb6bd9b9668c40437a4c51bd0d
Guto Marzagão <gutomarzagao@gmail.com>
2019-03-27 11:41:23 -0300

    Create project

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const port = 3000
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json({status: 'Server is running!'})
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`)
+})
